82ce88a8ffb702e5595f70ffcc876b370792ffd8
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-08-06 18:18:01 +0800

    added progress bar to show progress

--- static/main.js
+++ static/main.js
@@ -5,31 +5,36 @@
     'use strict';
     angular.module('HupuScraper', []).controller('ScraperController', ['$scope', '$log', '$http', '$timeout',
         function($scope, $log, $http, $timeout) {
+            $scope.submitButtonText = '运行';
+            $scope.loading = false;
             $scope.getResults = function () {
-              // fire API request
-              $http.post('/scrape', {}).
-                success(function (results) {
-                  $log.log(results);
-                  getData(results);
-                }).
-                error(function (error) {
-                  $log.log(error);
+                // fire API request
+                $http.post('/scrape', {}).success(function(results) {
+                    $log.log(results);
+                    getData(results);
+                    $scope.news = null;
+                    $scope.loading = true;
+                    $scope.submitButtonText = '运行中';
+                }).error(function (error) {
+                    $log.log(error);
                 });
             };
+
             function getData(jobID) {
-                var timeout = "";
+                var timeout = '';
                 var poller = function () {
-                  $http.get('/results/'+jobID).
-                    success(function(data, status, headers, config) {
+                    $http.get('/results/'+jobID).success(function(data, status, headers, config) {
                         if(status === 202) {
-                          $log.log(data, status);
+                            $log.log(data, status);
                         } else if (status === 200){
-                          $log.log(data);
-                          $scope.news = data;
-                          $log.log('set news to data');
-                          $timeout.cancel(timeout);
-                          window.location.reload();
-                          return false;
+                            $log.log(data);
+                            $scope.loading = false;
+                            $scope.submitButtonText = "运行";
+                            $scope.news = data;
+                            $log.log('set news to data');
+                            $timeout.cancel(timeout);
+                            window.location.reload();
+                            return false;
                         }
                         timeout = $timeout(poller, 2000);
                     });
