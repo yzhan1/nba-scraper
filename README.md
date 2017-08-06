7f0569cd8ae032c4c762bce4ab696e93884d9508
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-08-06 12:24:06 +0800

    added route refresh

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 Simple flask application that scrapes all the news titles on HUPU's NBA section.
 
+Utilized Redis task queue to handle the scraping
+
 + User can download the scraped data as JSON file and use them for NLP training.
 
-+ Download the file and run ```python3 app.py```
++ To run the app:
 
-+ Route for corpus data in JSON format: ```/get-json```
+    +  Download the folder
+    
+    +  Install packages in requirement.txt using ```pip3 install```
+    
+    +  Run ```python3 app.py```
+
++ API route for corpus data in JSON format: ```/get-json```
 
 + Live at: https://powerful-cove-80847.herokuapp.com/
--- static/main.js
+++ static/main.js
@@ -2,39 +2,40 @@
  * Created by yaomingzhan on 2017/8/5.
  */
 (function () {
-  'use strict';
-  angular.module('HupuScraper', []).controller('ScraperController', ['$scope', '$log', '$http', '$timeout',
-      function($scope, $log, $http, $timeout) {
-          $scope.getResults = function () {
-            // fire API request
-            $http.post('/scrape', {}).
-              success(function (results) {
-                $log.log(results);
-                getData(results);
-              }).
-              error(function (error) {
-                $log.log(error);
-              });
-          };
-          function getData(jobID) {
-              var timeout = "";
-              var poller = function () {
-                $http.get('/results/'+jobID).
-                  success(function(data, status, headers, config) {
-                      if(status === 202) {
-                        $log.log(data, status);
-                      } else if (status === 200){
-                        $log.log(data);
-                        $scope.news = data;
-                        $log.log('set news to data');
-                        $timeout.cancel(timeout);
-                        return false;
-                      }
-                      timeout = $timeout(poller, 2000);
-                  });
-              };
-              poller();
-          }
-      }
-  ]);
+    'use strict';
+    angular.module('HupuScraper', []).controller('ScraperController', ['$scope', '$log', '$http', '$timeout',
+        function($scope, $log, $http, $timeout) {
+            $scope.getResults = function () {
+              // fire API request
+              $http.post('/scrape', {}).
+                success(function (results) {
+                  $log.log(results);
+                  getData(results);
+                }).
+                error(function (error) {
+                  $log.log(error);
+                });
+            };
+            function getData(jobID) {
+                var timeout = "";
+                var poller = function () {
+                  $http.get('/results/'+jobID).
+                    success(function(data, status, headers, config) {
+                        if(status === 202) {
+                          $log.log(data, status);
+                        } else if (status === 200){
+                          $log.log(data);
+                          $scope.news = data;
+                          $log.log('set news to data');
+                          $timeout.cancel(timeout);
+                          window.location.reload();
+                          return false;
+                        }
+                        timeout = $timeout(poller, 2000);
+                    });
+                };
+                poller();
+            }
+        }
+    ]);
 }());
