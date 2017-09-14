ec23addd2adededf3d6d57a247923f3aa1363bc2
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-09-14 00:50:13 -0400

    fixing heroku

--- static/main.js
+++ static/main.js
@@ -1,13 +1,13 @@
 /**
  * Created by yaomingzhan on 2017/8/5.
  */
-(function () {
+(function() {
     'use strict';
     angular.module('HupuScraper', []).controller('ScraperController', ['$scope', '$log', '$http', '$timeout',
         function($scope, $log, $http, $timeout) {
             $scope.submitButtonText = '运行';
             $scope.loading = false;
-            $scope.getResults = function () {
+            $scope.getResults = function() {
                 // fire API request
                 $http.post('/scrape', {}).success(function(results) {
                     $log.log(results);
@@ -15,18 +15,18 @@
                     $scope.news = null;
                     $scope.loading = true;
                     $scope.submitButtonText = '运行中';
-                }).error(function (error) {
+                }).error(function(error) {
                     $log.log(error);
                 });
             };
 
             function getData(jobID) {
                 var timeout = '';
-                var poller = function () {
+                var poller = function() {
                     $http.get('/results/'+jobID).success(function(data, status, headers, config) {
-                        if(status === 202) {
+                        if (status === 202) {
                             $log.log(data, status);
-                        } else if (status === 200){
+                        } else if (status === 200) {
                             // $log.log(data);
                             $scope.loading = false;
                             $scope.submitButtonText = "运行";
